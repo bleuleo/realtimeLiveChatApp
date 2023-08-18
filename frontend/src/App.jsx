1bd368d0c322d4939c1eda06b92eaea692d069df
leo <leo@example.com>
2023-08-18 21:04:57 +0800

    Seperated Login and register page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,40 @@
 import { useState } from "react";
-
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
 
 import AuthPage from "./AuthPage";
 import ChatsPage from "./ChatsPage";
+import RegisterPage from "./RegisterPage";
 
 function App() {
   const [user, setUser] = useState(undefined);
 
-  if (!user) {
-    return <AuthPage onAuth={(user) => setUser(user)} />;
-  } else {
-    return <ChatsPage user={user} />;
-  }
+  return (
+    <Router>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            user ? (
+              <ChatsPage user={user} />
+            ) : (
+              <AuthPage onAuth={(user) => setUser(user)} />
+            )
+          }
+        />
+        <Route
+          path="/register"
+          element={
+            user ? (
+              <ChatsPage user={user} />
+            ) : (
+              <RegisterPage onAuth={(user) => setUser(user)} />
+            )
+          }
+        />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
--- frontend/src/RegisterPage.jsx
+++ frontend/src/RegisterPage.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import axios from "axios";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
-const AuthPage = (props) => {
+const RegisterPage = (props) => {
   const [username, setUsername] = useState();
   const [secret, setSecret] = useState();
   // const [email, setEmail] = useState();
@@ -64,7 +64,7 @@ const AuthPage = (props) => {
     <div className="login-page">
       <div className="card">
         {/* Login Form */}
-        <form onSubmit={onLogin}>
+        {/* <form onSubmit={onLogin}>
           <div className="title">Login</div>
           {loginError && <div className="error">{loginError}</div>}
           <input
@@ -80,12 +80,58 @@ const AuthPage = (props) => {
             onChange={(e) => setSecret(e.target.value)}
           />
           <button type="submit">LOG IN</button>
-          <Link to="/Register">Create an Account?</Link>
+        </form> */}
+
+        {/* Sign Up Form */}
+        <form onSubmit={onSignup}>
+          <div className="title">Sign Up</div>
+          {signupError && <div className="error">{signupError}</div>}{" "}
+          {/* Show the signup error message */}
+          {usernameTakenError && (
+            <div className="error">{usernameTakenError}</div>
+          )}{" "}
+          {/* Show the username taken error message */}
+          <input
+            type="text"
+            name="username"
+            placeholder="Username*"
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <input
+            type="password"
+            name="secret"
+            placeholder="Password*"
+            onChange={(e) => setSecret(e.target.value)}
+          />
+          {/* {emailError && <div className="error">{emailError}</div>} 
+          <input
+            type="text"
+            name="email"
+            placeholder="Email*"
+            onChange={(e) => setEmail(e.target.value)}
+          /> */}
+          {signupFirstNameError && (
+            <div className="error">{signupFirstNameError}</div>
+          )}
+          <input
+            type="text"
+            name="first_name"
+            placeholder="First name*"
+            onChange={(e) => setFirstName(e.target.value)}
+          />
+          <input
+            type="text"
+            name="last_name"
+            placeholder="Last name"
+            onChange={(e) => setLastName(e.target.value)}
+          />
+          <button type="submit">SIGN UP</button>
+          <Link to="/">Already Have an Account? Login</Link>
         </form>
       </div>
 
       <style>{`
-      .login-page { width: 100vw; height: 100vh; padding-top: 6vw; background: linear-gradient(180deg, rgba(117,84,160,1) 7%, rgba(117,84,160,1) 17%, rgba(106,95,168,1) 29%, rgba(99,103,174,1) 44%, rgba(87,116,184,1) 66%, rgba(70,135,198,1) 83%, rgba(44,163,219,1) 96%, rgba(22,188,237,1) 100%, rgba(0,212,255,1) 100%); }
+      .login-page { width: 100vw; height: 100vh; padding-top: 6vw; background: linear-gradient(180deg, #5d467c 7%, #b18ae4 17%, #786dbb 29%, rgba(99,103,174,1) 44%, #496cbe 66%, rgba(70,135,198,1) 83%, rgba(44,163,219,1) 96%, rgba(22,188,237,1) 100%, rgba(0,212,255,1) 100%); }
       .card { width: 200px; position: relative; left: calc(50vw - 100px); text-align: center; }
       .title { padding-top: 32px; font-size: 22px; color: white; font-weight: 700; }
       input { width: calc(100% - 16px); margin-top: 12px; padding: 8px; background-color: #e6f7ff; outline: none; border: 1px solid #e6f7ff; }
@@ -96,4 +142,4 @@ const AuthPage = (props) => {
   );
 };
 
-export default AuthPage;
+export default RegisterPage;
